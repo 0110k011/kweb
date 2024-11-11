fc7277ff6989f86a70bd7927726aec97fc1a134b
0110k011 <gmateust@gmail.com>
2024-11-11 20:33:45 -0300

    start add file management

---
+++ src/main/java/com/api/kweb/controllers/FileController.java
@@ -0,0 +1,28 @@
+package com.api.kweb.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/file")
+public class FileController {
+
+    @Autowired
+    private FileService fileService;
+
+    @PostMapping("/upload-ofx")
+    public ResponseEntity<OfxDataDto> uploadOfxFile(@RequestParam("file") MultipartFile file) {
+        try {
+            OfxDataDto dto = fileService.processOfxFile(file);
+            return ResponseEntity.ok(dto);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
