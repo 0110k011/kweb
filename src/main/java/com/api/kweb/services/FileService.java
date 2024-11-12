3396a48988c9cb48e58d468bbafc4fd270df8c75
0110k011 <gmateust@gmail.com>
2024-11-12 20:55:52 -0300

    fix file service

--- src/main/java/com/api/kweb/services/FileService.java
+++ src/main/java/com/api/kweb/services/FileService.java
@@ -1,30 +1,8 @@
 package com.api.kweb.services;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-import com.webcohesion.ofx4j.domain.data.banking.BankTransactionList;
-import com.webcohesion.ofx4j.domain.data.common.Transaction;
-
-import java.io.InputStream;
-import java.util.ArrayList;
 
 @Service
 public class FileService {
 
-    public OfxDataDto processOfxFile(MultipartFile file) throws Exception {
-        InputStream inputStream = file.getInputStream();
-
-        OfxParser parser = new OfxParser();
-        OfxData ofxData = parser.parse(inputStream);
-
-        OfxDataDto dto = new OfxDataDto();
-        dto.setBankName(ofxData.getBankName());
-        dto.setAccountNumber(ofxData.getAccountNumber());
-
-        List<TransactionDto> transactions = new ArrayList<>();
-        for (Transaction transaction : ofxData.getTransactions()) {
-            TransactionDto transactionDto = new TransactionDto();
-            transactionDto.setTransactionId(transaction.getId());
-        }
-    }
 }
